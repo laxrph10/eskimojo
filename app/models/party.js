783da45d6f42ccb490a6ab633dfeab608477c51f
Robert Horrigan <roberthorrigan@gmail.com>
2015-03-02 11:15:01 -0500

    Create party and song models

---
+++ app/models/party.js
@@ -0,0 +1,32 @@
+
+// # party
+
+var jsonSelect = require('mongoose-json-select');
+var mongoosePaginate = require('mongoose-paginate');
+
+exports = module.exports = function(mongoose, iglooMongoosePlugin) {
+
+  var Party = new mongoose.Schema({
+    name: {
+      type: String,
+      required: true
+    }
+  });
+
+  // virtuals
+  Party.virtual('object').get(function() {
+    return 'party';
+  });
+
+  // plugins
+  //Party.plugin(jsonSelect, '-_group -salt -hash');
+  Party.plugin(mongoosePaginate);
+
+  // keep last
+  Party.plugin(iglooMongoosePlugin);
+
+  return mongoose.model('Party', Party);
+};
+
+exports['@singleton'] = true;
+exports['@require'] = [ 'igloo/mongo', 'igloo/mongoose-plugin' ];
